80abce1adae136d680b22f4f5c8f3ed7da13b8c4
Oladele <oladele@gmail.com>
2015-12-18 13:52:39 -0500

    Initial network-site-listing as component

---
+++ tests/integration/components/network-site-listing-test.js
@@ -0,0 +1,21 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+moduleForComponent('network-site-listing', 'Integration | Component | network site listing', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  var networkSite = Ember.Object.create({name: 'ACME Lab'});
+  console.log('network-site-listing-test networkSite:', networkSite);
+  this.set('networkSite', networkSite);
+
+  this.render(hbs`
+    {{network-site-listing networkSite=networkSite}}
+  `);
+
+  assert.equal(this.$('[data-role=network-site-link]:first').text(), "ACME Lab");
+
+});
