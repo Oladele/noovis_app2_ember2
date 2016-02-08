cd17661d7ef5eb51767534d8054dff5b030d5cba
Oladele <oladele@gmail.com>
2016-02-08 06:24:54 -0500

    Use CRL data headings for table headings

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -1,58 +1,39 @@
 import Ember from 'ember';
 import ColumnDefinition from 'ember-table/models/column-definition';
-// import {randomNumber, randomDate} from '../utils/random';
+import {getData, getDataHeadings} from './stats_data_TEMP';
 
 export default Ember.Controller.extend({
 
-  columnNames: Ember.computed(function() {
-    return [
-      {
-        source: "open",
-        target: "Open"
-      },
-      {
-        source: "high",
-        target: "High"
-      },
-      {
-        source: "low",
-        target: "Low",
-      },
-      {
-        source: "close",
-        target: "Close"
-      }
-    ];
-  }),
+  tableHeadings: getDataHeadings(),
+  tableData: getData(),
 
-  tableColumns: Ember.computed('columnNames',function() {
-    const columnNames = this.get("columnNames");
-    const columnDefinitions = [];
-    
-    columnNames.forEach(function(columnName){
-      const column = ColumnDefinition.create({
-        savedWidth: 100,
-        headerCellName: columnName.target,
-        getCellContent: function(row) {
-          return row.get(columnName.source);
-        }
-      });
-      columnDefinitions.pushObject(column)
+  tableColumn: function(columnName){
+    const column = ColumnDefinition.create({
+      savedWidth: 100,
+      headerCellName: columnName.target,
+      getCellContent: function(row) {
+        return row.get(columnName.source);
+      }
+    });
+    return(column);
+  },
+
+  tableColumns: Ember.computed('tableHeadings',function() {
+    const tableHeadings = this.get("tableHeadings");
+    const columns = [];
+    const getColumn = this.get("tableColumn");
+
+    tableHeadings.forEach(function(columnName){
+      const column = getColumn(columnName);
+      columns.pushObject(column);
     });
 
-    return(columnDefinitions);
+    return(columns);
   }),
 
   tableContent: Ember.computed(function() {
-    var content = [];
-    for (var i = 0; i < 100; i++) {
-      content.pushObject({
-        open:  55,
-        high:  60,
-        low:  70,
-        close:  80,
-      });
-    }
+    const content = this.get("tableData");
+    console.log("content: ", content);
     return content;
   })
 });
