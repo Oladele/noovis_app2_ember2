4805fabd5391a55abbafea4f0b1a84edc6560f26
Oladele <oladele@gmail.com>
2016-02-23 08:42:42 -0500

    Remove unused stats-bar implementation from stats

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -1,26 +1,4 @@
 import Ember from 'ember';
-import five_ranges from './stats-bar-data-TEMP';
+
 export default Ember.Controller.extend({
-
-  // ---------
-  // Default Settings
-  // ---------
-  betweenGroupPadding: 1,
-  withinGroupPadding: 0,
-  maxLabelHeight: 40,
-  stackBars: true,
-  hasXAxisTitle: false,
-  hasYAxisTitle: false,
-  xValueDisplayName: 'X Axis',
-  yValueDisplayName: 'Y Axis',
-
-  data: Ember.computed('selectedData', 'rawDataHash', function() {
-    return this.get('rawDataHash')[this.get('selectedData')];
-  }),
-  rawDataHash: Ember.computed(function() {
-    return {
-      five_ranges: five_ranges,
-    };
-  }),
-  selectedData: 'five_ranges'
 });
