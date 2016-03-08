195df3dd8cb7beba48228be7f191c8aaf6d5a167
Steven Wu <wusteven@gmail.com>
2016-03-07 17:39:22 -0800

    Fix notifications to send only once
    Checks are added for `responseText` to prevent exceptions when
    running Mirage.

--- app/components/http-request/component.js
+++ app/components/http-request/component.js
@@ -5,10 +5,14 @@ import layout from 'emberx-xml-http-request/templates/components/x-xml-http-requ
 export default Component.extend({
   layout: layout,
 
+  // TODO: Consider removing checks for `responseText`.
+  // Only applies for Mirage responses.
   sendResponse: Ember.observer('model.reponse', function() {
-    let message = this.get('model.response.message');
+    let hasResponseText = Object.keys(this.get('model.xhr')).includes('responseText');
+    let responseText = hasResponseText ? JSON.parse(this.get('model.xhr.responseText')) : '';
+    let message = responseText ? responseText.message : this.get('model.response.message');
     let status = this.get('model.status');
-    if (status || message) {
+    if (status && message) {
       this.get('onResponse')(status, message);
     }
   })
