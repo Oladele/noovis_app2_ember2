bdaf55741e88b89472bf0755df78906b4a2faefb
Steven Wu <wusteven@gmail.com>
2016-03-20 22:54:47 -0700

    Move uploader to CRL Uploads tab

--- app/routes/sites/buildings/edit/crl-uploads.js
+++ app/routes/sites/buildings/edit/crl-uploads.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 
+const {
+  RSVP
+} = Ember;
+
 export default Ember.Route.extend({
   model() {
     let building = this.modelFor('sites.buildings.edit').building;
-    return building.get('sheets');
+    return RSVP.hash({
+      building,
+      sheets: building.get('sheets')
+    });
   }
 });
