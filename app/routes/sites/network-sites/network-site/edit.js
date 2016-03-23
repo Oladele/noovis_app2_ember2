fdf14e4ca069883a56e5bbf5ce32d4d02899af94
Steven Wu <wusteven@gmail.com>
2016-03-23 11:29:38 -0700

    Show network site node counts

--- app/routes/sites/network-sites/network-site/edit.js
+++ app/routes/sites/network-sites/network-site/edit.js
@@ -1,6 +1,4 @@
 import Ember from 'ember';
-import ColumnDefinition from 'ember-table/models/column-definition';
-import Stats from 'noovis-app2-ember2/helpers/_networkSites-edit-data';
 
 const {
   RSVP,
@@ -10,22 +8,10 @@ const {
 export default Ember.Route.extend({
   ajax: inject.service(),
   model() {
-    // let ajax = this.get('ajax');
-    // let siteId = this.paramsFor('sites.network-sites.network-site').id;
     return RSVP.hash({
       site: this.modelFor('sites.network-sites.network-site'),
-      buildings: this.modelFor('sites.network-sites.network-site').get('buildings'),
-      // tableContent: ajax.request(`/network-sites/${siteId}/stats-content`),
-      // tableColumns: ajax.request(`/network-sites/${siteId}/stats-columns`)
-      // TODO: remove stub
-      tableContent: Stats.columns,
-      tableColumns: Stats.headers
+      buildings: this.modelFor('sites.network-sites.network-site').get('buildings')
     });
-  },
-
-  afterModel(model) {
-    let tableColumns = createTableColumns(model.tableColumns);
-    model.tableColumns = tableColumns;
   },
 
   actions: {
@@ -54,15 +40,3 @@ export default Ember.Route.extend({
     }
   }
 });
-
-function createTableColumns(types) {
-  return types.map(type => {
-    return ColumnDefinition.create({
-      savedWidth: 100,
-      headerCellName: type,
-      getCellContent(row) {
-        return row.get(type);
-      }
-    });
-  });
-}
