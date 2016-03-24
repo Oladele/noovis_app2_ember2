2926a19b06f17611829a2c0cd1257ec10ea8c05e
Steven Wu <wusteven@gmail.com>
2016-03-24 14:53:13 -0700

    Fix filtered graph to include parent nodes

--- app/controllers/sites/buildings/edit/network.js
+++ app/controllers/sites/buildings/edit/network.js
@@ -6,14 +6,32 @@ const {
 } = Ember;
 
 export default Ember.Controller.extend({
-  filteredGraph: computed('filterIds', 'model.nodes.[]', 'model.edges.[]', function() {
-    let ids = this.get('filterIds');
+  runNodes: computed('model.nodes.[]', 'filterIds', function() {
     let nodes = this.get('model.nodes');
-    let edges = this.get('model.edges');
-    if (!isEmpty(ids)) {
-      nodes = nodes.filter(node => ids.includes(node.cable_run_id.toString()));
+    let ids = this.get('filterIds');
+
+    return nodes.filter(node => {
+      return ids.includes(node.cable_run_id.toString());
+    });
+  }),
+
+  filteredGraph: computed('filterIds', 'model.nodes.[]', 'model.edges.[]', function() {
+    let { nodes, edges } = this.get('model');
+    let ids = this.get('filterIds');
+    if (isEmpty(ids)) {
+      return { edges, nodes };
     }
-    return { edges, nodes };
+
+    let branch = this.get('runNodes');
+    if (isEmpty(branch)) {
+      return { edges, nodes };
+    }
+
+    let shallowestNode = findShallowestNode(branch);
+    let ancestors = findAllAncestors(shallowestNode, nodes);
+    branch.push.apply(branch, ancestors);
+
+    return { edges, nodes: branch };
   }),
 
   actions: {
@@ -22,3 +40,17 @@ export default Ember.Controller.extend({
     }
   }
 });
+
+function findShallowestNode(tree) {
+  return tree.sortBy('node_level')[0];
+}
+
+function findAllAncestors(node, tree) {
+  let nodes = [];
+  while (node.node_level > 1) {
+    let parent = tree.findBy('id', node.parent_id);
+    nodes.push(parent);
+    node = parent;
+  }
+  return nodes;
+}
