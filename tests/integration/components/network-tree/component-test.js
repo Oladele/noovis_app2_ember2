ed97d09e7c318f1a70abd8cf246b3b294c5519e7
Steven Wu <wusteven@gmail.com>
2016-04-07 13:44:54 -0700

    Remove unused test

--- tests/integration/components/network-tree/component-test.js
+++ tests/integration/components/network-tree/component-test.js
@@ -5,24 +5,6 @@ moduleForComponent('network-tree', 'Integration | Component | network tree', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });"
-
-  this.render(hbs`{{network-tree}}`);
-
-  assert.ok(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#network-tree}}
-      template block text
-    {{/network-tree}}
-  `);
-
-  assert.ok(this.$().text().trim(), 'template block text');
-});
-
 test('should have container div', function(assert) {
   this.set('edges', []);
   this.set('nodes', []);
