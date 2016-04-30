6dcb556418007a77ec3ba50d7967fc6bb99e417a
Steven Wu <wusteven@gmail.com>
2016-04-30 14:54:38 -0700

    Improve login UI
    * show error message for invalid credentials
    * disable login button when authentication is processing
    * show spinner on login button during authentication

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,19 +1,31 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+import { task } from 'ember-concurrency';
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   session: Ember.inject.service(),
 
+  authenticateTask: task(function * (email, password) {
+    let authenticator = 'authenticator:application';
+    let session = this.get('session');
+    let credentials = {
+      password,
+      identification: email,
+    };
+    yield session.authenticate(authenticator, credentials)
+      .catch(({ errors }) => { 
+        let flashMessages = this.get('flashMessages');
+        flashMessages.warning(errors[0])
+      });
+  }).drop(),
+
   actions: {
+    didTransition() {
+      this.controller.set('authenticateTask', this.get('authenticateTask'));
+    },
+
     authenticate(email, password) {
-      let authenticator = 'authenticator:application';
-      let session = this.get('session');
-      let credentials = {
-        password,
-        identification: email,
-      };
-      session.authenticate(authenticator, credentials)
-        .catch(({ error }) => this.set('error', error));
+      this.get('authenticateTask').perform(email, password);
     }
   }
 });
