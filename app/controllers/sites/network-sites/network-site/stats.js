7d31d6bc03bd09caa4a813f61903bfaa389695c3
Steven Wu <wusteven@gmail.com>
2016-05-29 23:24:28 -0700

    Add network chart selection animations

--- app/controllers/sites/network-sites/network-site/stats.js
+++ app/controllers/sites/network-sites/network-site/stats.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
+const {
+  computed
+} = Ember;
+
 export default Ember.Controller.extend({
+  queryParams: ['chart'],
+  chart: 'pon-channel',
+  showPonChannel: computed.equal('chart', 'pon-channel'),
+  showFeederCapacity: computed.equal('chart', 'feeder-capacity'),
+  showDistributionNetwork: computed.equal('chart', 'distribution-network')
 });
