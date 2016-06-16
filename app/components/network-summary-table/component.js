726ea4b7f3d98b12fced594fcedab09aa897a66e
Steven Wu <wusteven@gmail.com>
2016-06-15 23:15:39 -0700

    Add sortable, filterable network summary table

--- app/components/network-summary-table/component.js
+++ app/components/network-summary-table/component.js
@@ -1,9 +1,27 @@
 import Ember from 'ember';
 
+const {
+  computed,
+} = Ember;
+
 export default Ember.Component.extend({
+  columns: computed('headers', function() {
+    let headers = this.get('headers');
+    let editColumn = {
+      propertyName: '',
+      title: 'Edit',
+      template: 'tables/cell-edit-run'
+    };
+    let results = [editColumn];
+    let rest = headers.map(header => ({
+      propertyName: header,
+      title: header
+    }));
+    return results.concat(rest);
+  }),
+
   actions: {
     selectRow(id) {
-      this.set('selectedId', id);
       this.get('onSelect')(id);
     }
   }
--- app/routes/sites/buildings/edit/network.js
+++ app/routes/sites/buildings/edit/network.js
@@ -5,19 +5,31 @@ const {
   RSVP
 } = Ember;
 
+const emptyGraph = {
+  data: {
+    id: null,
+    attributes: {
+      nodes: [],
+      edges: []
+    }
+  }
+};
+
 export default Ember.Route.extend({
   ajax: inject.service(),
   model() {
     let id = this.paramsFor('sites.buildings.edit').building_id;
     let { building } = this.modelFor('sites.buildings.edit');
-    let cableRuns = building.get('sheets')
+    let latestSheet = building.get('sheets')
       .then(sheets => sheets.reload())
-      .then(newSheets => newSheets.get('lastObject.cableRuns'));
+      .then(newSheets => newSheets.get('lastObject'));
+    let cableRuns = latestSheet.then(sheet => sheet.get('cableRuns'));
     let graph = this.get('ajax').request(`buildings/${id}/latest_network_graph`)
       .then(results => results, () => emptyGraph);
 
     return RSVP.hash({
       building,
+      latestSheet,
       cableRuns,
       graph
     })
@@ -33,12 +45,3 @@ export default Ember.Route.extend({
   }
 });
 
-const emptyGraph = {
-  data: {
-    id: null,
-    attributes: {
-      nodes: [],
-      edges: []
-    }
-  }
-};
