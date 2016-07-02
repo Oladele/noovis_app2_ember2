0d3bde339759fd70c4a1713dd551813fb9dd13b9
Steven Wu <wusteven@gmail.com>
2016-07-01 23:00:25 -0700

    Link site building markers to their own maps

--- app/components/g-maps-container/component.js
+++ app/components/g-maps-container/component.js
@@ -9,12 +9,13 @@ export default Ember.Component.extend({
   lng: -95.677068,
   zoom: 4,
   markers: computed.map('model', function(marker) {
+    let url = `/sites/buildings/${marker.get('id')}/map`;
     return {
       id: marker.get('id'),
       lat: marker.get('lat'),
       lng: marker.get('lng'),
       infoWindow: {
-        content: marker.get('name')
+        content: `<a href=\"${url}\">${marker.get('name')}</a>`
       },
       icon: this.get('icon')
     };
